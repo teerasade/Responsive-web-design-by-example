90ea205963d86f06aadbb4ec647623bcc908bcdf
teerasade <teerasade.kh@gmail.com>
2018-05-10 23:20:59 +0700

    first commit

---
+++ Chapter03/index.php
@@ -0,0 +1,9 @@
+<?php require_once( "snippets/header.php" ); ?>
+
+<div class="container">
+  <div class="row">
+    <h1>Hello, world!</h1>
+  </div>
+</div>
+
+<?php require_once( "snippets/footer.php"); ?>
